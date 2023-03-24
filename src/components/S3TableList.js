527cfd89c04ff6af5b0186db14a18321bcbb1f35
michaelbulaongdev <michaelbulaongdev@gmail.com>
2023-03-24 08:40:32 +0800

    s3table checkpoint

---
+++ src/components/S3TableList.js
@@ -0,0 +1,68 @@
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+
+const check = <CheckCircleIcon className='check-icon' />;
+const rtu = 'Ready-to-use';
+const ao = 'Add-on; can be purchased separately';
+
+export const tableList = [
+	{
+		id: 1,
+		col1: 'Completion of paperwork for submission',
+		col2: check,
+	},
+	{
+		id: 2,
+		col1: 'Completion of paperwork for submission',
+		col2: rtu,
+	},
+	{
+		id: 3,
+		col1: 'Completion of paperwork for submission',
+		col2: ao,
+	},
+	{
+		id: 4,
+		col1: 'Completion of paperwork for submission',
+		col2: check,
+	},
+	{
+		id: 5,
+		col1: 'Completion of paperwork for submission',
+		col2: check,
+	},
+	{
+		id: 6,
+		col1: 'Completion of paperwork for submission',
+		col2: check,
+	},
+	{
+		id: 7,
+		col1: 'Completion of paperwork for submission',
+		col2: check,
+	},
+	{
+		id: 8,
+		col1: 'Completion of paperwork for submission',
+		col2: check,
+	},
+	{
+		id: 9,
+		col1: 'Completion of paperwork for submission',
+		col2: check,
+	},
+	{
+		id: 10,
+		col1: 'Completion of paperwork for submission',
+		col2: check,
+	},
+	{
+		id: 11,
+		col1: 'Completion of paperwork for submission',
+		col2: check,
+	},
+	{
+		id: 12,
+		col1: 'Completion of paperwork for submission',
+		col2: check,
+	},
+];
--- src/components/Section3.js
+++ src/components/Section3.js
@@ -1,13 +1,31 @@
 import {Box} from '@mui/material';
+import {tableList} from './S3TableList';
 
 export default function Section3() {
 	return (
-		<section>
+		<section className='section3'>
 			<Box sx={{maxWidth: 'md', p: 3, m: '0 auto'}}>
-				<table>
-					<th>
-						<h3>PACKAGE INCLUSIONS</h3>
-					</th>
+				<table className='s3table'>
+					<tr>
+						<th className='s3table-col1'>
+							<h3>PACKAGE INCLUSIONS</h3>
+						</th>
+						<th className='s3table-col2'>
+							<div>
+								<p>RTO2GO DIY</p>
+								<button>Get Started</button>
+							</div>
+						</th>
+						<th className='s3table-col3'>{null}</th>
+					</tr>
+					{tableList.map((item) => {
+						return (
+							<tr className='s3table-row' key={item.id}>
+								<td className='s3table-col1'>{item.col1}</td>
+								<td className='s3table-col2'>{item.col2}</td>
+							</tr>
+						);
+					})}
 				</table>
 			</Box>
 		</section>
